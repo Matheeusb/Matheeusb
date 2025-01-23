6ef521c159a150d58fb84292e75c0a04f7f9bd72
Matheus <matheus_bereta@hotmail.com>
2025-01-23 13:36:16 -0300

    :recycle: Resolving bug about base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nome-do-seu-repositorio/',
+  base: `/${process.env.VITE_BASE_URL}/`,
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
